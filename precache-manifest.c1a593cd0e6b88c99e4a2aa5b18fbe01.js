94c9fadc744a3c9a6093adad8fdcd96ad3339ac0
HarryHq <mhuanyum@gmail.com>
2018-12-16 21:06:39 +0800

    Updates

---
+++ precache-manifest.c1a593cd0e6b88c99e4a2aa5b18fbe01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "35a587f98cc35803ad5366862ba2bb96",
+    "url": "/static/media/bg.35a587f9.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "765fe529f89aa93c2d0f",
+    "url": "/static/js/main.765fe529.chunk.js"
+  },
+  {
+    "revision": "50157d8d436d7efe68b7",
+    "url": "/static/js/1.50157d8d.chunk.js"
+  },
+  {
+    "revision": "765fe529f89aa93c2d0f",
+    "url": "/static/css/main.1af7c058.chunk.css"
+  },
+  {
+    "revision": "50157d8d436d7efe68b7",
+    "url": "/static/css/1.c17584ac.chunk.css"
+  },
+  {
+    "revision": "16ec2ca2fcb02de22d9a6f1a09390190",
+    "url": "/index.html"
+  }
+];
